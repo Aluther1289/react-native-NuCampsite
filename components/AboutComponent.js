5cf9f5524c3d597806232f707a790cf5b71b35cb
Adam Luther <aluther1289@gmail.com>
2021-01-04 17:29:19 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native-gesture-handler";
-import { PARTNERS } from "../shared/partners";
 import { Card, ListItem } from "react-native-elements";
-import { Text, FlatList } from "react-native";
+import { ScrollView, Text, FlatList } from "react-native";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+
+const mapStateToProps = (state) => {
+	return {
+		partners: state.partners,
+	};
+};
 
 function Mission() {
 	return (
@@ -22,13 +28,6 @@ function Mission() {
 }
 
 class About extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			partners: PARTNERS,
-		};
-	}
-
 	static navigationOptions = {
 		title: "About Us",
 	};
@@ -40,7 +39,7 @@ class About extends Component {
 					title={item.name}
 					subtitle={item.description}
 					leftAvatar={{
-						source: require("./images/bootstrap-logo.png"),
+						source: { uri: baseUrl + item.image },
 					}}
 				/>
 			);
@@ -51,7 +50,7 @@ class About extends Component {
 				<Mission />
 				<Card title="Community Partners">
 					<FlatList
-						data={this.state.partners}
+						data={this.props.partners.partners}
 						keyExtractor={(item) => item.id.toString()}
 						renderItem={renderPartner}
 					/>
@@ -61,4 +60,4 @@ class About extends Component {
 	}
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -17,6 +17,20 @@ import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Icon } from "react-native-elements";
 import SafeAreaView from "react-native-safe-area-view";
+import { connect } from "react-redux";
+import {
+	fetchCampsites,
+	fetchComments,
+	fetchPromotions,
+	fetchPartners,
+} from "../redux/ActionCreators";
+
+const mapDispatchToProps = {
+	fetchCampsites,
+	fetchComments,
+	fetchPartners,
+	fetchPromotions,
+};
 
 const AboutNavigator = createStackNavigator(
 	{
@@ -212,6 +226,13 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+	componentDidMount() {
+		this.props.fetchCampsites();
+		this.props.fetchComments();
+		this.props.fetchPartners();
+		this.props.fetchPromotions();
+	}
+
 	render() {
 		return (
 			<View
@@ -258,4 +279,4 @@ const styles = StyleSheet.create({
 	},
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
