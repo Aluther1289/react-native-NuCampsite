44f745941e7aa7954a5c2ba051dbf5a1934b1295
Adam Luther <aluther1289@gmail.com>
2021-01-10 11:19:55 -0500

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from "./ContactComponent";
 import Directory from "./DirectoryComponent";
 import Reservation from "./ReservationComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
+import Favorites from "./FavoritesComponent";
 import {
 	View,
 	Platform,
@@ -32,6 +33,31 @@ const mapDispatchToProps = {
 	fetchPartners,
 	fetchPromotions,
 };
+
+const FavoritesNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Favorites },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+			headerLeft: (
+				<Icon
+					name="heart"
+					type="font-awesome"
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
 
 const AboutNavigator = createStackNavigator(
 	{
@@ -221,6 +247,20 @@ const MainNavigator = createDrawerNavigator(
 				drawerIcon: ({ tintColor }) => (
 					<Icon
 						name="tree"
+						type="font-awesome"
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
+		Favorites: {
+			screen: FavoritesNavigator,
+			navigationOptions: {
+				drawerLabel: "My Favorites",
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name="heart"
 						type="font-awesome"
 						size={24}
 						color={tintColor}
